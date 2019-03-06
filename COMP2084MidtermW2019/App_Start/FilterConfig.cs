0d4b52a55ed0cf33a430effe0d48577fb8c02ecc
Vasu Joshi <joshivasu5@gmail.com>
2019-03-06 12:50:15 -0500

    Initial commit

---
+++ COMP2084MidtermW2019/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2084MidtermW2019
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
+        }
+    }
+}
